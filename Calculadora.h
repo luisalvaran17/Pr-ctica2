c7586f8feebddbaed6ffdfcc13aaca6ec26b8676
ricardo0915 <valencia.ricardo@correomisena.edu.co>
2017-04-26 09:34:18 -0500

    update

---
+++ Calculadora.h
@@ -0,0 +1,31 @@
+/*
+ * Autor: Sebastian Delgado y Luis Albaran
+ * Descripción: Calculadora
+ * Fecha de modificación: 26 abril de Abril
+ */
+ 
+  #ifndef CALCULADORA_H
+  #define CALCULADORA_H
+  
+  using namespace std;
+ 
+class Calculadora {
+	private:
+		
+		int numero;
+	    double numero1;
+	    double numero2;
+	    double resultado
+		
+	public:
+		Calculadora();
+		~Calculadora();
+		
+		void suma ();
+		void resta();
+		void multiplicacion ();
+		void division ();
+		void Opciones ();
+		void Menu ();
+};
+#endif
